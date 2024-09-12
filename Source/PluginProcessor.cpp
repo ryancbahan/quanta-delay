795bf03ce87b295925d3241477f6929d34d95648
Ryan  Bahan <ryan@ryanbahan.com>
2024-09-11 18:48:20 -0600

    better params

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -272,8 +272,23 @@ void QuantadelayAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, 
             float rightOutput = delayedSampleRight;
             stereoManagers[i].process(leftOutput, rightOutput);
             
-            wetSignalLeft += leftOutput;
-            wetSignalRight += rightOutput;
+            const float tremRate = 0.25f / (spreadValue / 3);
+            const float tremDepth = 1.0f * (spreadValue / 5);
+            static float tremPhase = 0.0f;
+            const float tremPhaseInc = tremRate / getSampleRate();
+            
+            // Apply Harmonic Tremolo
+            float tremLfo = 0.5f + 0.5f * sinf(2.0f * M_PI * tremPhase);
+            float lowPass = leftOutput * (1.0f - (tremDepth) * (tremLfo * 3)) + rightOutput * (tremDepth * tremLfo);
+            
+            float highPass = leftOutput * (tremDepth * tremLfo) + rightOutput * (1.0f - tremDepth * tremLfo);
+            
+            wetSignalLeft += lowPass;
+            wetSignalRight += highPass;
+            
+            // Update tremolo phase
+            tremPhase += tremPhaseInc;
+            if (tremPhase >= 1.0f) tremPhase -= 1.0f;
         }
 
         // Add partial contribution from the transitioning delay line
@@ -303,25 +318,10 @@ void QuantadelayAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, 
         // Scale the wet signal by the current (smoothed) number of delay lines
 //        wetSignalLeft /= currentDelayLines;
 //        wetSignalRight /= currentDelayLines;
-        
-        const float tremRate = 0.25f / spreadValue;
-        const float tremDepth = 1.0f * spreadValue;
-        static float tremPhase = 0.0f;
-        const float tremPhaseInc = tremRate / getSampleRate();
-        
-        // Apply Harmonic Tremolo
-        float tremLfo = 0.5f + 0.5f * sinf(2.0f * M_PI * tremPhase);
-        float lowPass = wetSignalLeft * (1.0f - (tremDepth) * (tremLfo * 3)) + wetSignalRight * (tremDepth * tremLfo);
-        
-        float highPass = wetSignalLeft * (tremDepth * tremLfo) + wetSignalRight * (1.0f - tremDepth * tremLfo);
 
         // Combine dry and wet signals
-        leftChannel[sample] = inputSampleLeft + mixValue * lowPass;
-        rightChannel[sample] = inputSampleRight + mixValue * highPass;
-        
-        // Update tremolo phase
-        tremPhase += tremPhaseInc;
-        if (tremPhase >= 1.0f) tremPhase -= 1.0f;
+        leftChannel[sample] = inputSampleLeft + mixValue * wetSignalLeft;
+        rightChannel[sample] = inputSampleRight + mixValue * wetSignalRight;
     }
 }
 
