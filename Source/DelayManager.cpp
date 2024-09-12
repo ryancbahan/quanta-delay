26cdb048ad27a0bcf174a048c9bcd00220fd65b6
Ryan  Bahan <ryan@ryanbahan.com>
2024-09-11 20:21:08 -0600

    remove pitch shift from delay manager

--- Source/DelayManager.cpp
+++ Source/DelayManager.cpp
@@ -46,25 +46,5 @@ float DelayManager::processSample(float inputSample)
     float feedbackSample = delayedSample * currentFeedback;
     delayLine.pushSample(0, inputSample + feedbackSample);
     
-    // Pitch shifting
-    static float readPos = 0.0f;
-    float pitchShiftFactor = 2.0f;  // Octave up
-    float readIncrement = 1.0f / pitchShiftFactor;
-    
-    float pitchShiftedSample = 0.0f;
-    for (int i = 0; i < pitchShiftFactor; ++i)
-    {
-        float fractionalDelay = readPos + i * readIncrement;
-        fractionalDelay = std::fmod(fractionalDelay, currentDelayTime);
-        
-        float sample = delayLine.popSample(0, fractionalDelay);
-        pitchShiftedSample += sample;
-    }
-    pitchShiftedSample /= pitchShiftFactor;  // Normalize
-    
-    readPos += 1.0f;
-    if (readPos >= pitchShiftFactor)
-        readPos -= pitchShiftFactor;
-    
-    return pitchShiftedSample;
+    return delayedSample;
 }
